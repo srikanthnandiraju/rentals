54a15625128dbde1c34ef076b3257a5e09db685a
Srikanth <nandiraju@yahoo.com>
2018-06-24 09:28:27 -0700

    Rent for a Cause

---
+++ js/app.js
@@ -0,0 +1,58 @@
+var App = {};
+
+// Global Event Bus to fire and listen to events
+App.Events = {
+    emit: function(event_name, event_payload) {
+        console.log("PUBED");
+        $('body').trigger(event_name, event_payload);
+    },
+    listen: function(event_name, callback) {
+        $('body').unbind(event_name).bind(event_name, function(e, data) {
+            callback(data);
+        });
+    }
+};
+
+App.FullScreen = {
+    show: function(screenurl) {
+        $("#fullscreen_stage").load(screenurl);
+        $('#fullscreenElement').addClass('open');
+    },
+    hide: function() {
+        $('#fullscreenElement').removeClass('open');
+    }
+};
+
+App.init = function() {
+    NProgress.configure({
+        showSpinner: false
+    });
+
+    $(document).ajaxStart(function() {
+        NProgress.start();
+    });
+
+    $(document).ajaxStop(function() {
+        NProgress.done();
+    });
+
+    $('.close').on('click', function(event) {
+        $('#fullscreenElement').removeClass('open');
+    });
+
+    // Side Nav Instance
+    App.sidenav = new Sidenav({
+        content: document.getElementById("content"),
+        sidenav: document.getElementById("sidenav"),
+        backdrop: document.getElementById("backdrop")
+    });
+};
+
+
+// Generic events to handle side bar visibility
+App.Events.listen("hide-side-nav", function() {
+    App.sidenav.close();
+});
+App.Events.listen("show-side-nav", function() {
+    App.sidenav.open();
+});
---
+++ js/routes.js
@@ -0,0 +1,24 @@
+Path.map("#/logout").to(function() {
+    App.FullScreen.show("screens/login.html");
+});
+Path.map("#/login").to(function() {
+    App.FullScreen.show("screens/login.html");
+});
+
+Path.map("#/home").to(function() {
+    $("#title-text").html("Welcome");
+    $("#stage").load("screens/home.html");
+});
+
+Path.map("#/test").to(function() {
+    //App.FullScreen.show("screens/search.html");
+    $("#stage").load("screens/speaker.html");
+});
+
+Path.map("#/speaker").to(function() {
+    //App.FullScreen.show("screens/search.html");
+    $("#stage").load("screens/speaker.html");
+});
+
+Path.root("#/home");
+Path.listen();
