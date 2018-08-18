b748932a1b72a7d3fd12109fbd75205cfd0c6505
Srikanth <nandiraju@yahoo.com>
2018-08-18 11:34:14 -0700

    More Screens

--- js/routes.js
+++ js/routes.js
@@ -17,9 +17,14 @@ Path.map("#/test").to(function() {
     $("#stage").load("screens/signup.html");
 });
 
-Path.map("#/speaker").to(function() {
-    //App.FullScreen.show("screens/search.html");
-    $("#stage").load("screens/speaker.html");
+Path.map("#/newrental").to(function() {
+    $("#title-text").html("New Rental");
+    $("#stage").load("screens/newrental.html");
+});
+
+Path.map("#/rentalhistory").to(function() {
+    $("#title-text").html("Rental History");
+    $("#stage").load("screens/rentalhistory.html");
 });
 
 Path.root("#/login");
